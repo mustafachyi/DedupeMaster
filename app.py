67cc3a2e9639867d3124f1d07ec9d94d51bd77eb
mustafachyi <154711815+mustafachyi@users.noreply.github.com>
2023-12-27 23:20:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_socketio import SocketIO, emit
 from threading import Thread
 import stripe
 import random
 import string
 import time
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///keys.db'
 db = SQLAlchemy(app)
 Bootstrap(app)
+socketio = SocketIO(app)
 
 class Key(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,7 +44,7 @@ def create_random_stripe_key(prefix, length):
     return prefix + ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 
 def generate_and_check_keys():
-    verified_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+    verified_key = os.getenv('STRIPE_API_KEY')
     if not validate_stripe_key(verified_key):
         print("Unable to validate the API. Please check your verified key.")
         return
@@ -64,6 +67,7 @@ def generate_and_check_keys():
                 new_key = Key(key=stripe_key)
                 db.session.add(new_key)
                 db.session.commit()
+            socketio.emit('new_key', {'key': stripe_key}, namespace='/test')
 
 Thread(target=generate_and_check_keys).start()
 
@@ -71,6 +75,10 @@ Thread(target=generate_and_check_keys).start()
 def home():
     keys = Key.query.all()
     return render_template('index.html', keys=keys)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my_response', {'data': 'Connected'})
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=10000)
